e136672f8db8dd61562fab8723d93ac426d86809
Daniel Rudolf <github.com@daniel-rudolf.de>
2019-08-04 21:51:11 +0200

    Cleanup headers of templates/{403,404,500}.php
    Signed-off-by: Daniel Rudolf <github.com@daniel-rudolf.de>

--- templates/403.php
+++ templates/403.php
@@ -23,20 +23,13 @@
 /** @var $_ array */
 /** @var $l \OCP\IL10N */
 /** @var $theme OCP\Defaults */
-// @codeCoverageIgnoreStart
-if(!isset($_)) {//standalone  page is not supported anymore - redirect to /
-	require_once '../../lib/base.php';
 
-	$urlGenerator = \OC::$server->getURLGenerator();
-	header('Location: ' . $urlGenerator->getAbsoluteURL('/'));
-	exit;
-}
-// @codeCoverageIgnoreEnd
 ?>
+
 <div class="body-login-container update">
 	<div class="icon-big icon-error icon-white"></div>
 	<h2><?php p($l->t('Access forbidden')); ?></h2>
-	<p class="infogroup"><?php if(isset($_['message'])) p($_['message'])?></p>
+	<p class="infogroup"><?php if (isset($_['message'])) p($_['message'])?></p>
 	<p><a class="button primary" href="<?php p(\OC::$server->getURLGenerator()->linkTo('', 'index.php')) ?>">
 		<?php p($l->t('Back to %s', array($theme->getName()))); ?>
 	</a></p>
--- templates/500.php
+++ templates/500.php
@@ -23,20 +23,13 @@
 /** @var $_ array */
 /** @var $l \OCP\IL10N */
 /** @var $theme OCP\Defaults */
-// @codeCoverageIgnoreStart
-if(!isset($_)) {//standalone  page is not supported anymore - redirect to /
-	require_once '../../lib/base.php';
 
-	$urlGenerator = \OC::$server->getURLGenerator();
-	header('Location: ' . $urlGenerator->getAbsoluteURL('/'));
-	exit;
-}
-// @codeCoverageIgnoreEnd
 ?>
+
 <div class="body-login-container update">
 	<div class="icon-big icon-error icon-white"></div>
 	<h2><?php p($l->t('Internal Server Error')); ?></h2>
-	<p class="infogroup"><?php if(isset($_['message'])) { p($_['message']); } else { p($l->t('The server was unable to complete your request.')); } ?></p>
+	<p class="infogroup"><?php if (isset($_['message'])) { p($_['message']); } else { p($l->t('The server was unable to complete your request.')); } ?></p>
 	<p class="infogroup"><?php p($l->t('If this happens again, please send the technical details below to the server administrator.')) ?></p>
 
 	<ul class="infogroup">
