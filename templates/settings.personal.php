35108b3d606eaf7d21bf2a66139dbb752211e044
rakekniven <mark.ziegler@rakekniven.de>
2021-03-07 11:13:02 +0100

    Fixed typo
    Reported at Transifex.
    
    Signed-off-by: rakekniven <mark.ziegler@rakekniven.de>

--- templates/settings.personal.php
+++ templates/settings.personal.php
@@ -426,7 +426,7 @@ style(Application::APP_NAME, 'pico');
 
 		<script id="picocms-website-form-error" type="text/template">
 			<p><?php p($l->t(
-				'A unexpected error occurred while performing this action. Please check Nextcloud\'s logs.'
+				'An unexpected error occurred while performing this action. Please check Nextcloud\'s logs.'
 			)); ?></p>
 			<p class="error-details" style="display: none">
 				<?php p($l->t('Error: {error}')); ?>
