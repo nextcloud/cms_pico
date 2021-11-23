651084f042a6e74e1ec94e2a60340228fef0414b
Valdnet <47037905+Valdnet@users.noreply.github.com>
2021-11-23 10:44:36 +0100

    l10n: Spelling unification
    Signed-off-by: Valdnet <47037905+Valdnet@users.noreply.github.com>

--- templates/settings.admin.php
+++ templates/settings.admin.php
@@ -618,7 +618,7 @@ style(Application::APP_NAME, 'pico');
 					</label>
 				</p>
 				<p class="note"><?php p($l->t(
-					'After you\'ve configured your webserver to enable shortened URLs, you should select the '
+					'After you have configured your web server to enable shortened URLs, you should select the '
 							. 'corresponding option above to let your users know about this feature. Don\'t enable '
 							. 'this option if you have not configured the virtual "sites/" folder yet using one of the '
 							. 'configuration examples shown below.'
